47eb9cb9d8618ac7e786459a731267a97261c50e
João Sartori <53714290+jsartori12@users.noreply.github.com>
2025-02-13 02:01:08 -0300

    Add files via upload

--- cdr_numbering.py
+++ cdr_numbering.py
@@ -1,10 +1,19 @@
-
+#https://abnumber.readthedocs.io/en/latest/#position
 from abnumber import Chain
 
 
-seq = 'QVQLQQSGAELARPGASVKMSCKASGYTFTRYTMHWVKQRPGQGLEWIGYINPSRGYTNYNQKFKDKATLTTDKSSSTAYMQLSSLTSEDSAVYYCARYYDDHYCLDYWGQGTTLTVSSAKTTAPSVYPLA'
+seq = 'QVQLKQSGPGLVQPSQSLSITCTVSGFSLTNYGVHWVRQSPGKGLEWLGVIWSGGNTDYNTPFTSRLSINKDNSKSQVFFKMNSLQSNDTAIYYCARALTYYDYEFAYWGQGTLVTVSAASTKGPSVFPLAPSSKSTSGGTAALGCLVKDYFPEPVTVSWNSGALTSGVHTFPAVLQSSGLYSLSSVVTVPSSSLGTQTYICNVNHKPSNTKVDKRVEPK'
 
 chain = Chain(seq, scheme='imgt', cdr_definition='imgt')
+
+
+pipi = pyrosetta.rosetta.core.pose.get_resnums_for_chain(starting_pose, "D")
+''.join(starting_pose.sequence()[i-1] for i in pipi)
+
+
+#### pedir pro usuario passar a cadeia pesada e leve. dai, o algoritmo extrai a sequencia ^
+#### identifica as cdrs e fm, e pega os index. baseado no input, ele adiciona esses index
+#### na lista de fixados para otimizados
 
 chain.cdr1_seq
 chain.cdr2_seq
